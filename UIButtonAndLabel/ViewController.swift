7c69e14a0015d938e7ad2794bb9dda30e2658122
VNavrockij <vitalik.nav@gmail.com>
2022-05-03 17:49:23 +0300

    feat: new task

--- UIButtonAndLabel/ViewController.swift
+++ UIButtonAndLabel/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func pressedBtn(_ sender: UIButton) {
+        label.text = "Hello"
+    }
+    
 }
 
